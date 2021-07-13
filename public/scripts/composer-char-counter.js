c55e7f67dc12a0bcac2746898f5ae4bb18ad5f71
Roshanak Akbarifar <roshanak.akbarifar@gmail.com>
2021-07-13 17:25:37 -0400

    showing tweets in the timeline

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,11 +2,9 @@ $(document).ready(function() {
   $("#tweet-text").keyup(function() {
     $(".counter").html(140 - this.value.length);
     if (this.value.length > 140) {
-      $(".counter").addClass('counterRed');
-      $(".counter").removeClass('counterBlack');
+      $(".counter").addClass('counterRed').removeClass('counterBlack');
     } else {
-      $(".counter").addClass('counterBlack');
-      $(".counter").removeClass('counterRed');
+      $(".counter").addClass('counterBlack').removeClass('counterRed');
     }
   });
 });
