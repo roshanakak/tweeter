bba9a7764f41ad57a26ac4aab9e08f3356c36b6d
Roshanak Akbarifar <roshanak.akbarifar@gmail.com>
2021-07-14 11:38:58 -0400

    tweets are rendered synamically

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,51 +7,63 @@
 
 $(document).ready(function() {
 
-const createTweetElement = function(tweetObj) {
-  return `<article>
-            <header>
+  // Fake data taken from initial-tweets.json
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
+
+  const createTweetElement = function(tweetObj) {
+    return `<article>
+              <header>
+                  <div>
+                    <img src=${tweetObj.user.avatars} alt="Avatar" class="avatar">
+                    ${tweetObj.user.name}
+                  </div>
+                  <label>${tweetObj.user.handle}</label>
+              </header>
+              <content>
+                <div>${tweetObj.content.text}</div>
+              </content>
+              <footer>
+                <label>${timeago.format(tweetObj.created_at)}</label>
                 <div>
-                  <img src=${tweetObj.user.avatars} alt="Avatar" class="avatar">
-                  ${tweetObj.user.name}
+                  <i class="fas fa-flag"></i>
+                  <i class="fas fa-retweet"></i>
+                  <i class="fas fa-heart"></i>
                 </div>
-                <label>${tweetObj.user.handle}</label>
-            </header>
-            <content>
-              <div>${tweetObj.content.text}</div>
-            </content>
-            <footer>
-              <label>${tweetObj.created_at}</label>
-              <div>
-                <i class="fas fa-flag"></i>
-                <i class="fas fa-retweet"></i>
-                <i class="fas fa-heart"></i>
-              </div>
-            </footer>
-          </article>
-          <br/>`;
-
-};
-
-
-
-// Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
-};
-
-const $tweet = createTweetElement(tweetData);
-
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('#tweets-container').append($tweet);
-
-
+              </footer>
+            </article>
+            <br/>`;
+  
+  };
+  
+  const renderTweets  = function(tweetObjArray) {
+    tweetObjArray.forEach(element => {
+      const $tweet = createTweetElement(element);
+      $('#tweets-container').append($tweet);
+    });
+  
+  };
+  renderTweets(data);
 });
