0e5f4bdeabf83a4356e525b47d71bb541dc2dc05
Roshanak Akbarifar <roshanak.akbarifar@gmail.com>
2021-07-15 13:02:44 -0400

    load tweets with ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,31 +1,5 @@
 $(document).ready(function() {
 
-  // Fake data taken from initial-tweets.json
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
-
   const createTweetElement = function(tweetObj) {
     return `<article>
               <header>
@@ -55,14 +29,20 @@ $(document).ready(function() {
     tweetObjArray.forEach(element => {
       const $tweet = createTweetElement(element);
       $('#tweets-container').append($tweet);
-    });
-  
+    });  
   };
-  renderTweets(data);
 
   $(".new-tweet").submit(function(event) {
     event.preventDefault();
-    $.post("/tweets/", $("#tweet-text").serialize());
+    $.post("/tweets", $("#tweet-text").serialize());
   });
+
+  const loadtweets = function() {  
+    $.getJSON('/tweets', function(tweetsdata) {
+      renderTweets(tweetsdata);
+    });
+  };
+
+  loadtweets();
 
 });
